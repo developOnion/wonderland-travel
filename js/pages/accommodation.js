0045fae1bf7cf5e85f5882d3ebce44c4bf43844c
developOnion <smeyonion.nou@gmail.com>
2025-12-19 11:05:46 +0700

    Add carousel for all sections in accommodation page

--- js/pages/accommodation.js
+++ js/pages/accommodation.js
@@ -1,7 +1,9 @@
 import { initNavbar } from "../components/navbar.js";
+import { initCarousel } from "../components/carousel.js";
 
 function initAccommodationPage() {
     initNavbar();
+    initCarousel();
 }
 
 document.addEventListener("DOMContentLoaded", initAccommodationPage);
