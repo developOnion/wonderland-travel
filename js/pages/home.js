79bdb5d017acd3beb8898d933a26cb8fb0db4611
developOnion <smeyonion.nou@gmail.com>
2025-12-19 11:38:41 +0700

    Add carousel for all section in home page

--- js/pages/home.js
+++ js/pages/home.js
@@ -1,7 +1,9 @@
 import { initNavbar } from "../components/navbar.js";
+import { initCarousel } from "../components/carousel.js";
 
 function initHomePage() {
     initNavbar();
+    initCarousel();
 }
 
 document.addEventListener("DOMContentLoaded", initHomePage);
