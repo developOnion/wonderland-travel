4dbce5b3fed8eca04427de9f4985aecb54813645
developOnion <smeyonion.nou@gmail.com>
2025-12-25 19:37:51 +0700

    Add carousel functionality to transport.html

--- js/pages/transport.js
+++ js/pages/transport.js
@@ -1,7 +1,9 @@
 import { initNavbar } from "../components/navbar.js";
+import { initCarousel } from "../components/carousel.js";
 
 function initTransport() {
     initNavbar();
+    initCarousel();
 }
 
 document.addEventListener("DOMContentLoaded", initTransport);
